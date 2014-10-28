3844f2b7a69c918bab4962dd7cddebe62f85cfa3
Kenny Huang <Khuangtai@gmail.com>
2014-10-28 02:10:06 -0400

    add subscriptions and moderators to subs association, being construct controller and views

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,2 +1,53 @@
 class SubsController < ApplicationController
+  before_action :required_sign_in
+
+  def index
+    @subs = Sub.all
+  end
+
+  def new
+    @sub = Sub.new
+  end
+
+  def create
+    @sub = Sub.new(subs_params)
+
+    if @sub.save
+      redirect_to subs_url
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @sub = Sub.find(params[:id]);
+  end
+
+  def edit
+    @sub = Sub.find(params[:id])
+  end
+
+  def update
+    @sub = Sub.find(params[:id])
+    if @sub.update(Subs_params)
+      redirect_to sub_url(@sub)
+    else
+      flash[:errors] = @sub.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @sub = Sub.find(params[:id]);
+    @sub.destroy
+    redirect_to subs_url
+  end
+
+
+  private
+  def subs_params
+    params.require(:sub).permit(:name, :title, :description);
+  end
+
 end
