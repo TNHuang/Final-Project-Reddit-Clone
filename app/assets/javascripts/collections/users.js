702d50de45a6f3575668258daba161ef7bd34ab8
Kenny Huang <khuangtai@gmail.com>
2014-12-03 17:21:55 -0500

    fix user collection and model

---
+++ app/assets/javascripts/collections/users.js
@@ -0,0 +1,23 @@
+RedditClone.Collections.Users = Backbone.Collection.extend({
+  url: "api/users",
+  model: RedditClone.Models.User,
+
+  // // parsePost: function (resposen) {
+  // //   //take out the response and set it to an attribute
+  // // }
+  // getOrFetch: function (id) {
+  //   var user = this.get(id);
+  //   if (!user) {
+  //     user = new RedditClone.Models.User({id: id})
+  //     user.fetch({
+  //       success: function () {
+  //         this.add(user);
+  //       }.bind(this)
+  //     });
+  //   }else{
+  //     user.fetch();
+  //   }
+  //   return user;
+  // },
+
+});
