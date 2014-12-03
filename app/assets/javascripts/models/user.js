de16ed2ccab2b1fbae3691fa3e3c3c3bd248fb22
Kenny Huang <khuangtai@gmail.com>
2014-12-03 17:00:23 -0500

    disabling user show page js for now

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,43 +1,43 @@
 RedditClone.Models.User = Backbone.Model.extend({
   urlRoot: "/api/users",
-
-  posts: function () {
-    if(!this._posts) {
-      this._posts = new RedditClone.Collections.Posts([], { user: this });
-    }
-
-    return this._posts;
-  },
-
-  modSubs: function () {
-    if(!this._modSubs) {
-      this._modSubs = new RedditClone.Collections.Subs([], { user: this });
-    }
-    return this._modSubs;
-  },
-
-  subs: function () {
-    if(!this._subs) {
-      this._subs = new RedditClone.Collection.Subs([], { user: this})
-    }
-    return this._mods;
-  },
-
-  parse: function (response) {
-    if(response.posts) {
-      this.posts().set(response.posts, { parse: true });
-      delete response.posts;
-    }
-    if (response.modSubs){
-      this.modSubs().set(response.modSubs, { parse: true });
-      delete response.modSubs;
-    }
-
-    if (response.subs){
-      this.subs().set(response.subs, { parse: true });
-      delete response.subs;
-    }
-    return response;
-  }
+  // 
+  // posts: function () {
+  //   if(!this._posts) {
+  //     this._posts = new RedditClone.Collections.Posts([], { user: this });
+  //   }
+  //
+  //   return this._posts;
+  // },
+  //
+  // modSubs: function () {
+  //   if(!this._modSubs) {
+  //     this._modSubs = new RedditClone.Collections.Subs([], { user: this });
+  //   }
+  //   return this._modSubs;
+  // },
+  //
+  // subs: function () {
+  //   if(!this._subs) {
+  //     this._subs = new RedditClone.Collection.Subs([], { user: this})
+  //   }
+  //   return this._mods;
+  // },
+  //
+  // parse: function (response) {
+  //   if(response.posts) {
+  //     this.posts().set(response.posts, { parse: true });
+  //     delete response.posts;
+  //   }
+  //   if (response.modSubs){
+  //     this.modSubs().set(response.modSubs, { parse: true });
+  //     delete response.modSubs;
+  //   }
+  //
+  //   if (response.subs){
+  //     this.subs().set(response.subs, { parse: true });
+  //     delete response.subs;
+  //   }
+  //   return response;
+  // }
 
 })
