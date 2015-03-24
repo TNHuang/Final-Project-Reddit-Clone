00fa211b3d02ac32335370386ee7b59e40fe7547
Kenny Huang <KHuangtai@gmail.com>
2015-03-24 14:01:38 -0400

    fix user redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     
     if @user.save
       sign_in(@user)
-      redirect_to "api/subs"
+      redirect_to "#/subs"
       # redirect_to user_url(@user)
     else
       flash.now[:errors] = @user.errors.full_messages
